6fcc489febf71be4365185a2ad1ba6732c66a886
Frol <frol@ewadev.com>
2011-04-29 21:28:02 +0300

    pretty output

--- quering_jsonfield/utils.py
+++ quering_jsonfield/utils.py
@@ -4,4 +4,4 @@ from django.utils import simplejson
 class JSONDict(dict):
     
     def __repr__(self):
-        return unicode(simplejson.dumps(self))
+        return unicode(simplejson.dumps(self, indent=4))
